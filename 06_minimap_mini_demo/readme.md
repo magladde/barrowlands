0fb201b83444bebbc6beb50bb1a71e7cb11848d0
morgan.g <magladde@gmail.com>
2025-09-02 22:06:43 -0400

    Implements a basic minimap demo
    Creates a working example of a minimap with separate layers for icons representing points of interest.
    
    The minimap displays icons for both the ship and a planet, enhancing user awareness of the game environment.
    
    This implementation follows the tutorial from https://medium.com/@merxon22/godot-create-your-first-2d-minimap-c43dfda01802.

---
+++ 06_minimap_mini_demo/readme.md
@@ -0,0 +1,3 @@
+tutorial taken from the following site:
+	https://medium.com/@merxon22/godot-create-your-first-2d-minimap-c43dfda01802
+	
---
+++ 06_minimap_mini_demo/scenes/ship.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+@export var thrust := 400.0
+@export var rotation_speed := 10.0
+@export var friction := 2.0
+@export var brake_strength := 300
+
+var braking := false
+var speed := 0.0
+
+func _physics_process(delta):
+	speed = velocity.length()
+	##### ship movement #####
+	
+	# flag to handle breaking
+	braking = Input.is_action_pressed("reverse_thrust")
+
+	# user input for turning
+	if Input.is_action_pressed("ui_left"):
+		rotation -= rotation_speed * delta
+	if Input.is_action_pressed("ui_right"):
+		rotation += rotation_speed * delta
+
+	# user input for thrust
+	if Input.is_action_pressed("thrust"):
+		velocity += Vector2.UP.rotated(rotation) * thrust * delta
+
+	# slow ship down with either of these two methods
+	if braking:
+		velocity = velocity.move_toward(Vector2.ZERO, brake_strength * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+
+	# Apply velocity to position
+	move_and_slide()
